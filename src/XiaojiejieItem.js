8746b5e5b722cdb1ebaf8bd82327a74e0a014bbc
rx-app <rxapp@yandex.com>
2019-11-07 19:45:22 +0800

    构造函数constructor中绑定this方法

--- src/XiaojiejieItem.js
+++ src/XiaojiejieItem.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react'; 
 class XiaojiejieItem extends Component{
+    constructor(props){
+        super(props)
+        this.handleClick = this.handleClick.bind(this)
+    }
     render(){
         return(
-            <div onClick={this.handleClick.bind(this)}>{this.props.content}</div>
+            <div onClick={this.handleClick}>{this.props.content}</div>
         );
     }
     handleClick(){
